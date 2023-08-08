4c9a70d8a6a4b13129a14f00d36ba28d96e8939d
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-08-08 09:00:18 +0300

    Update TopBar.tsx

--- app/components/shared/TopBar.tsx
+++ app/components/shared/TopBar.tsx
@@ -2,7 +2,30 @@ import Link from "next/link";
 import Image from "next/image";
 function TopBar(){
     return(
-        <div></div>
+        <nav className="topbar">
+            <Link
+                href={"/"}
+                className="flex items-center gap-4"
+            >
+                <Image
+                    src="/assets/logo.svg"
+                    alt="logo"
+                    width={28}
+                    height={28}
+                />
+                <p 
+                    className="text-heading3-bold text-light-1 max-xs:hidden">
+                        Threads
+                </p>
+            </Link>
+            <div 
+                className="flex items-center gap-1"
+            >
+                <div className="block md:hidden">
+
+                </div>
+            </div>
+        </nav>
     )
 }
 
