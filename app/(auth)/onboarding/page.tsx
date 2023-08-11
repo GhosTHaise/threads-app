c5ddebe226a5bdfc93d9bfd04a0830c50b67ec74
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-08-11 20:09:31 +0300

    Add AccountProfile

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,10 +1,21 @@
+import AccountProfile from "@/app/components/forms/AccountProfile";
 
 async function Page() {
     return(
-        <main>
+        <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
             <h1 className="head-text">
                 OnBoarding
             </h1>
+            <p
+                className="my-3 text-base-regular text-light-2"
+            >
+                Complete your profile now to use threads
+            </p>
+            <section
+                className="mt-9 bg-dark-2 p-10"
+            >
+                <AccountProfile />
+            </section>
         </main>
     )
 }
