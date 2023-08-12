c96cc41b5dcc33e250a4710bd1dd67183e1b9fd3
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-08-12 20:46:59 +0300

    Before using shcdn-ui

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,6 +1,17 @@
-import AccountProfile from "@/app/components/forms/AccountProfile";
-
+import AccountProfile from "@/components/forms/AccountProfile";
+import { currentUser } from "@clerk/nextjs";
 async function Page() {
+    const user = await currentUser();
+
+    const usrInfo = {};
+    const userData = {
+        id : user?.id,
+        objectId : userInfo?._id,
+        username : userInfo?.username || user?.username || "",
+        name : userInfo?.name || user?.firstName || "",
+        bio : userInfo?.bio || "",
+        image : userInfo?.image || user?.imageUrl
+    }
     return(
         <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
             <h1 className="head-text">
@@ -14,7 +25,10 @@ async function Page() {
             <section
                 className="mt-9 bg-dark-2 p-10"
             >
-                <AccountProfile />
+                <AccountProfile
+                    user={userData}
+                    btnTitle="Continue"
+                />
             </section>
         </main>
     )
--- app/components/forms/AccountProfile.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-const AccountProfile = () => {
-  return (
-    <div>AccountProfile</div>
-  )
-}
-
-export default AccountProfile
