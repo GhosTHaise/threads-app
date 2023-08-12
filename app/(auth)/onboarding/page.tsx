72037302aba218bdf784eeca7975bd21aa361fca
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-08-12 19:29:26 +0300

    init shadcn-ui

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -27,7 +27,10 @@ async function Page() {
             <section
                 className="mt-9 bg-dark-2 p-10"
             >
-                <AccountProfile />
+                <AccountProfile
+                    user={userData}
+                    btnTitle="Continue"
+                />
             </section>
         </main>
     )
