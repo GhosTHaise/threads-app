d26b38a6b0a03378baf725e5583610b0f41cc06b
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-08-17 18:13:05 +0300

    create update function

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,7 +1,37 @@
 "use server"
 
 import { connectToDb } from "../mongoose"
+import User from "../modals/user.model";
+import { revalidatePath } from "next/cache";
+export async function updateUser(
+    userId : string,
+    username : string,
+    name : string,
+    bio : string,
+    image : string,
+    path : string
+    ) : Promise<void>{
+    try {
+        connectToDb();
 
-export async function updateUser() : Promise<void>{
-    connectToDb();
+        await User.findOneAndUpdate(
+            { id : userId},
+            {
+                username : username.toLocaleLowerCase(),
+                name,
+                bio,
+                image,
+                onboarded : true
+            },
+            {
+                upsert : true
+            }
+        );
+
+        if(path === "/profile/edit"){
+            revalidatePath(path);
+        }
+    } catch (error : any) {
+        throw new Error(`Failed to create/update user : ${error.message}`)
+    }
 }
