dd6fdfa192d9b598b867464c753f425acaa720c3
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-08-18 18:41:33 +0300

    fetch user by id

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -1,4 +1,13 @@
+import { fetchUser } from "@/lib/actions/user.action";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
 async function Page(){
+    const user = await currentUser();
+    if(!user) return null;
+    const userInfo = await fetchUser(user.id);
+    console.log(userInfo);
+    
     return (
         <h1 className="head-text">
             Create Thread
