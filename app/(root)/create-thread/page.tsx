d36051c85e7843b343ecc8ea27c3ab4872a9fcf4
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-08-18 18:50:20 +0300

    create PostTread Component

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -1,3 +1,4 @@
+import PostThread from "@/components/forms/PostThread";
 import { fetchUser } from "@/lib/actions/user.action";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
@@ -6,12 +7,16 @@ async function Page(){
     const user = await currentUser();
     if(!user) return null;
     const userInfo = await fetchUser(user.id);
-    console.log(userInfo);
+    
+    if(!userInfo?.onboarded) redirect("/onboarding");
     
     return (
-        <h1 className="head-text">
-            Create Thread
-        </h1>
+        <>
+            <h1 className="head-text">
+                Create Thread
+            </h1>
+            <PostThread userId={userInfo._id} />
+        </>
     )
 }
 
---
+++ components/forms/PostThread.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostThread = ({userId} : {userId : String}) => {
+  return (
+    <h1>Post Thread Form</h1>
+  )
+}
+
+export default PostThread
