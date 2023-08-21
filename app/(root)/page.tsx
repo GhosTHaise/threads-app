59a0531d76206dcea75ebc6d38c5ce29fb119000
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-08-21 20:09:48 +0300

    get threads and use pagination

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,13 @@
+
 //app/page.tsx
+import { fetchPosts } from "@/lib/actions/thread.action";
 import { UserButton } from "@clerk/nextjs";
 //<UserButton afterSignOutUrl="/"/>
-export default function Home() {
+export default async function Home() {
+  const result = await fetchPosts(1,30);
+  //console.log(result);
+  
+  //if(posts)
   return (
     <>
       <h1 className="head-text text-left">
