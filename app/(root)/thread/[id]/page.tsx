3c047229776988cced1650b85a2760bd0e97f0e5
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-08-22 20:42:33 +0300

    create thread detail page

---
+++ app/(root)/thread/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = ({params} : {params : { id : string}}) => {
+  return (
+    <section className='relative'>
+        
+    </section>
+  )
+}
+
+export default Page
