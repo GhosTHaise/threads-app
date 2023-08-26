bbceb1c46303fda8f28722a8186cbb690d01f3a0
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-08-26 19:44:43 +0300

    Render profile image

--- components/shared/ProfileHeader.tsx
+++ components/shared/ProfileHeader.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface Props {
     accountId : string;
     authUserId : string;
@@ -16,10 +18,19 @@ const ProfileHeader = ({
     bio
 } : Props) => {
     return(
-        <div>
-            <h1>
-                Profile Header
-            </h1>
+        <div className="flex w-full flex-col justify-start">
+            <div className="flex items-center justify-between">
+                <div className="flex items-center gap-3">
+                    <div className="relative h-20 w-20 object-cover">
+                        <Image
+                            src={imgUrl}
+                            alt="profile url"
+                            fill
+                            className="rounded-full object-cover shadow-2xl"
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
