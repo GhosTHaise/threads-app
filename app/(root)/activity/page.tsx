867c9407036c3c810e9f61800c63999decc34663
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-09-02 19:05:35 +0300

    show activity text

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -39,6 +39,12 @@ const Page = async () => {
                           height={50}
                           className="rounded-full object-cover"
                       />
+                      <p className="!text-small-regular text-light-1">
+                        <span className="mr-1 text-primary-500">
+                          {activity.author.name}
+                        </span>{" "}
+                        replied to your thread.
+                      </p>
                     </article>
                   </Link>)
                 }
